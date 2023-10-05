226d69e495bcf7c6bd4eeead8cfbfcee34222654
Japapaz696 <Lucaspaz696@gmail.com>
2023-10-05 14:13:02 -0300

    .

---
+++ Exercicio3/java.js
@@ -0,0 +1,5 @@
+
+var nome = prompt("Digite Seu None") 
+var idade = prompt("Digite Sua Idade")
+
+document.write("<h1>Ola " + nome + " , Voce tem " +  idade +" anos</h1>")
---
+++ Exercicio4/js.js
@@ -0,0 +1,3 @@
+var num1 = prompt("Digite 1 numero")
+var num2 = prompt("Digite outro numero")
+document.write(num1 + num2 )
